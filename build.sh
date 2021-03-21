76af818d5f31959807721c436d8fba216c414a22
R503B <sodanak1n@outlook.com>
2021-03-21 19:12:48 +0100

    Test2

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ docker run --rm -i "${UIDARGS[@]}" -v $PWD/ffbuild:/ffbuild "$IMAGE" bash -s <<E
     cd /ffbuild
     rm -rf ffmpeg prefix
 
-    git clone https://github.com/richardpl/FFmpeg.git ffmpeg
+    git clone https://github.com/richardpl/ffmpeg/tree/bink2 ffmpeg
     cd ffmpeg
     git checkout $GIT_BRANCH
 
